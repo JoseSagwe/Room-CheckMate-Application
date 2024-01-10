ce36620f1c30f3ba1bb7243506837a19109b2c80
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-10 15:55:11 +0300

    login controller API

--- src/main/java/com/joseph/roomcheckmate/service/UserService.java
+++ src/main/java/com/joseph/roomcheckmate/service/UserService.java
@@ -4,14 +4,33 @@ import com.joseph.roomcheckmate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public int registerNewUserServiceMethod(String fname, String lname, String email, String password){
         return userRepository.registerNewUser(fname,lname,email,password);
     }  //End of Register New User
+
+    // Check User Email Services Method
+    public List<String> checkUserEmail(String email){
+        return userRepository.checkUserEmail(email);
+    }
+    // End of Check User Email Services Method
+
+    // Check User Password Services Method
+    public String checkUserPasswordByEmail(String email){
+        return userRepository.checkUserPasswordByEmail(email);
+    }
+    // End of Check User Password Services Method
+
 
 }
--- src/main/java/com/joseph/roomcheckmate/user_rest_contollers/LoginApiController.java
+++ src/main/java/com/joseph/roomcheckmate/user_rest_contollers/LoginApiController.java
@@ -1,16 +1,56 @@
 package com.joseph.roomcheckmate.user_rest_contollers;
 
+import com.joseph.roomcheckmate.models.Login;
+import com.joseph.roomcheckmate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class LoginApiController {
 
+    @Autowired
+    UserService userService;
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public LoginApiController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/login")
     public String login(){
         return "login";
     }
+
+    @PostMapping("/welcome")
+    public String authenticateUser(@RequestBody Login login){
+        //Get User Email
+        List<String> userEmail = userService.checkUserEmail(login.getEmail());
+        String hashed_password = userService.checkUserPasswordByEmail(login.getEmail());
+
+        //Check If Email is Empty
+        if(userEmail.isEmpty() || userEmail == null){
+            return new ResponseEntity("Email does not exist" , HttpStatus.NOT_FOUND);
+        }
+        // End of Check If Email is Empty
+
+        //Get Hashed user Password
+        //Validate the user password
+        if(!BCrypt.checkpw(login.getPassword(), hashed_password)){
+            return new ResponseEntity("Incorrect Username Or password" , HttpStatus.BAD_REQUEST);
+        }
+
+        //Set User Object
+        User user = userService.getUserDetailsByEmail(login.getEmail());
+        return new ResponseEntity(user, HttpStatus.OK);
+    }
+
 
 }
